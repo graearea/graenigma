b1087050c2290c0920e3efd64e1f62c001bfe99d
JaeRae <john.rae@springernature.com>
2020-08-07 13:19:53 +0100

    added filter for text only input, removed delete/replace and switched encoder to work on characters only

--- graenigma/src/main/kotlin/graenigma/Encoder.kt
+++ graenigma/src/main/kotlin/graenigma/Encoder.kt
@@ -1,29 +1,29 @@
 package graenigma
 
-import java.lang.RuntimeException
-
-class Encoder{
+class Encoder {
     private var loadedProgramme: Programme? = null
 
-    fun encode(input: String):String = loadedProgramme?.run { encode(input) } ?: throw RuntimeException("No Programme Loaded")
-    fun load(prog:Programme): Encoder {
-        loadedProgramme=prog
+    fun encode(input: Char): Char =
+        loadedProgramme?.run { encode(input) } ?: throw RuntimeException("No Programme Loaded")
+
+    fun load(prog: Programme): Encoder {
+        loadedProgramme = prog
         return this
     }
 }
 
 sealed class Programme {
-    abstract fun encode(input:String):String
-    class TypeWriter: Programme() {
-        override fun encode(input: String): String = input
+    abstract fun encode(input: Char): Char
+    class TypeWriter : Programme() {
+        override fun encode(input: Char): Char = input
     }
 
     class RunningKey : Programme() {
-        val mapping= "DMTWSILRUYQNKFEJCAZBPGXOHV"
-        override fun encode(input: String): String  {
-            return input.toLowerCase().map { c -> mapping[c.toInt() - 'a'.toInt()] }.joinToString("")
-
+        val mapping = "DMTWSILRUYQNKFEJCAZBPGXOHV"
+        override fun encode(input: Char): Char {
+            return mapping[input.toInt() - 'a'.toInt()]
         }
 
     }
+
 }
--- graenigma/src/main/kotlin/graenigma/Graenigma.kt
+++ graenigma/src/main/kotlin/graenigma/Graenigma.kt
@@ -9,52 +9,78 @@ import java.awt.event.KeyListener
 import javax.swing.JFrame
 import javax.swing.JScrollPane
 import javax.swing.JTextArea
+import javax.swing.text.AbstractDocument
+import javax.swing.text.AttributeSet
+import javax.swing.text.DocumentFilter
+
 
 fun main() {
     val frame = Graenigma()
-    frame.isVisible=true
+    frame.isVisible = true
 }
 
-class Graenigma :JFrame(){
+class Graenigma : JFrame() {
 
     val encoder = Encoder()
+
     init {
         createUI(title)
     }
 
     private fun createUI(title: String) {
-        this.layout=null
-            encoder.load(Programme.TypeWriter())
+        this.layout = null
+        encoder.load(Programme.TypeWriter())
         setTitle(title)
 
         val input = JTextArea()
-
+        (input.document as AbstractDocument).setDocumentFilter(MyFilter())
         val inputPane = JScrollPane(input)
-        inputPane.bounds= Rectangle(200,20,400,120)
+        inputPane.bounds = Rectangle(200, 20, 400, 120)
 
         val output = JTextArea()
         val outputPane = JScrollPane(output)
-        outputPane.bounds= Rectangle(200,400,400,120)
+        outputPane.bounds = Rectangle(200, 400, 400, 120)
+        val currentText = ""
 
-        val inputChangedListener = object : KeyListener{
-            override fun keyTyped(e: KeyEvent?) {
-                output.text = encoder.encode(input.text)
+        val inputChangedListener = object : KeyListener {
+            override fun keyTyped(e: KeyEvent) {
+                if (('a'.toInt()..'z'.toInt()).contains(e.keyChar.toInt())) {
+                    output.text = output.text + encoder.encode(e.keyChar)
+                }
+//                else input.text=input.text.substring(0,input.text.length-1)//grrr
             }
+
             override fun keyPressed(e: KeyEvent?) {}
             override fun keyReleased(e: KeyEvent?) {}
         }
         input.addKeyListener(inputChangedListener)
 
 
-        this.contentPane.add(inputPane,BorderLayout.CENTER)
-        this.contentPane.add(outputPane,BorderLayout.CENTER)
+        this.contentPane.add(inputPane, BorderLayout.CENTER)
+        this.contentPane.add(outputPane, BorderLayout.CENTER)
         defaultCloseOperation = EXIT_ON_CLOSE
         setSize(800, 600)
         setLocationRelativeTo(null)
     }
 
-    fun run() {
-        print("hello there")
-        JFrame()
+}
+
+internal class MyFilter : DocumentFilter() {
+    override fun replace(
+        fb: FilterBypass, offset: Int, length: Int,
+        text: String, attrs: AttributeSet?
+    ) {
+        if (offset < fb.document.length) {
+            return
+        }
+        if (('a'.toInt()..'z'.toInt()).contains(text[0].toInt())) {
+            super.replace(fb, offset, length, text, attrs)
+        }
     }
+
+
+    override fun insertString(fb: FilterBypass?, offset: Int, text: String, attr: AttributeSet?) {}
+
+
+    override fun remove(fb: FilterBypass, offset: Int, length: Int) {}
 }
