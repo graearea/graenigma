065337a4f57bb3629ae55a1b0568e974e96cf2c6
JaeRae <john.rae@springernature.com>
2020-08-07 11:59:56 +0100

    mapping test for simple case

--- graenigma/src/main/kotlin/graenigma/Encoder.kt
+++ graenigma/src/main/kotlin/graenigma/Encoder.kt
@@ -6,8 +6,9 @@ class Encoder{
     private var loadedProgramme: Programme? = null
 
     fun encode(input: String):String = loadedProgramme?.run { encode(input) } ?: throw RuntimeException("No Programme Loaded")
-    fun load(prog:Programme) {
+    fun load(prog:Programme): Encoder {
         loadedProgramme=prog
+        return this
     }
 }
 
@@ -15,5 +16,14 @@ sealed class Programme {
     abstract fun encode(input:String):String
     class TypeWriter: Programme() {
         override fun encode(input: String): String = input
+    }
+
+    class RunningKey : Programme() {
+        val mapping= "DMTWSILRUYQNKFEJCAZBPGXOHV"
+        override fun encode(input: String): String  {
+            return input.toLowerCase().map { c -> mapping[c.toInt() - 'a'.toInt()] }.joinToString("")
+
+        }
+
     }
 }
--- graenigma/src/test/kotlin/graenigma/MappingTest.kt
+++ graenigma/src/test/kotlin/graenigma/MappingTest.kt
@@ -2,10 +2,11 @@ package graenigma
 
 import org.junit.jupiter.api.Assertions.assertEquals
 
-internal class TypewriterTest {
+internal class MappingTest {
     @org.junit.jupiter.api.Test
     internal fun `should return whatever you give it`() {
-        assertEquals(Encoder().load(Programme.TypeWriter()).encode("aBc123!"),"aBc123!")
-        
+        //"DMTWSILRUYQNKFEJCAZBPGXOHV"
+        assertEquals(Encoder().load(Programme.RunningKey()).encode("abc"),"DMT")
     }
 }
+
